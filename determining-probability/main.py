de1b4c31c6621e26aed0949b3d6bd3f2940bfa03
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2024-12-07 17:56:00 +0100

    Created histograms of survival data

--- determining-probability/main.py
+++ determining-probability/main.py
@@ -1,5 +1,6 @@
 import csv
 import numpy as np
+import matplotlib.pyplot as plt
 
 with open("titanic.csv", "r") as file:
     data = csv.reader(file, delimiter=",")
@@ -19,8 +20,20 @@ for index in range(0, len(fare)):
   else:
       fare_not_survived.append(fare[index])
 
-print(f"The average fare of those who survived was: ${round(np.mean(fare_survived), 2)}")
-print(f"The average fare of those who did not survive was: ${round(np.mean(fare_not_survived), 2)}")
+bins = range(0, 240, 15)
 
-print(f"The median fare of those who survived was: ${round(np.median(fare_survived), 2)}")
-print(f"The median fare of those who did not survive was: ${round(np.median(fare_not_survived), 2)}")
+plt.hist(fare_not_survived, bins, histtype="bar", color="red", alpha=0.5)
+plt.hist(fare_survived, bins, histtype="bar", color="green", alpha=0.5)
+plt.xticks(range(0, 240, 20))
+plt.yticks(range(0, 360, 25))
+plt.xlabel("Fare Amount")
+plt.ylabel("Number of Passengers")
+plt.title("Fare Amount and Passenger Survival", fontsize=14)
+plt.gca().legend(("Did Not Survive", "Survived"))
+plt.savefig("fare-amount-passenger-survival.png")
+
+#print(f"The average fare of those who survived was: ${round(np.mean(fare_survived), 2)}")
+#print(f"The average fare of those who did not survive was: ${round(np.mean(fare_not_survived), 2)}")
+
+#print(f"The median fare of those who survived was: ${round(np.median(fare_survived), 2)}")
+#print(f"The median fare of those who did not survive was: ${round(np.median(fare_not_survived), 2)}")
