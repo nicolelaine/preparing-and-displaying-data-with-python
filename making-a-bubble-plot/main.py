69049fd92c5bdd20c0aa51f24ecbe8c9aa56c9ab
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2024-12-16 20:14:17 +0100

    Created a bubble plot on happiness and annual salary

--- making-a-bubble-plot/main.py
+++ making-a-bubble-plot/main.py
@@ -1,6 +1,7 @@
 import requests
 import pandas as pd
-
+import matplotlib.pyplot as plt 
+import seaborn as sns  
 
 # Standardizes currency to USD values so that we can better compare results
 def format_currency(dataset):
@@ -22,32 +23,35 @@ def format_currency(dataset):
 
 # ADD CODE: Pandas dataframes
 wage = pd.read_csv("wage.csv", delimiter=",")
-print(wage)
+#print(wage)
 happiness = pd.read_csv("happiness.csv", delimiter=",")
-print(happiness)
+#print(happiness)
 
 wage_usd = format_currency(wage)
-print(wage_usd)
+#print(wage_usd)
 
 wage_and_happiness = wage.merge(happiness)
-print(wage_and_happiness)
+#print(wage_and_happiness)
 wage_and_happiness_by_country = wage_and_happiness.groupby("Country")
-print(wage_and_happiness_by_country)
+#print(wage_and_happiness_by_country)
 wage_average_per_country = wage_and_happiness_by_country["Value"].mean()
 happiness_average_per_country = wage_and_happiness_by_country[
   "Happiness score"].mean()
 
-print("Countries with the highest average wages:",
-      wage_average_per_country.nlargest(10))
-print("Countries with the highest average happiness:",
-      happiness_average_per_country.nlargest(10))
-print("Countries with the lowest average wages:",
-      wage_average_per_country.nsmallest(10))
-print("Countries with the lowest average happiness:",
-      happiness_average_per_country.nsmallest(10))
+#print("Countries with the highest average wages:", wage_average_per_country.nlargest(10))
+#print("Countries with the highest average happiness:", happiness_average_per_country.nlargest(10))
+#print("Countries with the lowest average wages:", wage_average_per_country.nsmallest(10))
+#print("Countries with the lowest average happiness:", happiness_average_per_country.nsmallest(10))
 
 # With f-strings:
-print(f"Countries with the highest average wages:{wage_average_per_country.nlargest(10)}")
-print(f"Countries with the highest average happiness:{happiness_average_per_country.nlargest(10)}")
-print(f"Countries with the lowest average wages:{wage_average_per_country.nsmallest(10)}")
-print(f"Countries with the lowest average happiness:{happiness_average_per_country.nsmallest(10)}")
+#print(f"Countries with the highest average wages:{wage_average_per_country.nlargest(10)}")
+#print(f"Countries with the highest average happiness:{happiness_average_per_country.nlargest(10)}")
+#print(f"Countries with the lowest average wages:{wage_average_per_country.nsmallest(10)}")
+#print(f"Countries with the lowest average happiness:{happiness_average_per_country.nsmallest(10)}")
+
+fig = sns.scatterplot(x="Value", y="Happiness score", hue="Happiness score", size="Happiness score", sizes=(20, 180), data=wage_and_happiness)
+plt.title("Annual Salary and Happiness")
+plt.xlabel("Annual Salary of Full-Time Workers")
+plt.ylabel("Happiness Score of Citizens")
+fig.set_facecolor("#E5E5E5")
+plt.savefig("salary_and_happiness.png")
