eb1cae9ce36aa95aabb552639a6ed854847108cc
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2024-12-17 01:21:24 +0100

    Scraped the honey badger wiki

--- make-your-first-webscraper/main.py
+++ make-your-first-webscraper/main.py
@@ -5,3 +5,15 @@ honey_badger = requests.get("https://en.wikipedia.org/wiki/Honey_badger")
 honey_badger.raise_for_status()
 honey_badger_html = honey_badger.text.encode("utf-8")
 honey_badger_soup = BeautifulSoup(honey_badger_html, 'html.parser')
+
+h2 = honey_badger_soup.find_all("h2")
+print(len(h2))
+
+for i in h2:
+    print(i)
+  
+links = honey_badger_soup.find_all("a")
+print (links[:5])
+
+images = honey_badger_soup.find_all("img")
+print(images[-4:])
