21cb59259c767b2467f5eea3950aa4d329aa6c8b
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2024-12-29 16:06:03 +0100

    Created a heat map of tip size with time of day

--- creating-a-heatmap/main.py
+++ creating-a-heatmap/main.py
@@ -2,3 +2,22 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
+with open("tips.csv", "r") as csv_file:
+    tips = pd.read_csv(csv_file, delimiter=",")
+
+
+tips_pivoted = tips.pivot_table(
+    values="tip",
+    index=["size"],
+    columns=["time"]
+)
+
+fig = sns.heatmap(tips_pivoted, annot=True, cmap="coolwarm")
+
+fig.set_ylim(0,5)
+
+plt.xlabel("Meal Eaten")
+plt.ylabel("Party Size")
+plt.title("When are the biggest tips?")
+
+plt.savefig("time-tipsize.png")
