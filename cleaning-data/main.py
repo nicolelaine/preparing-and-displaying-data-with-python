25b78fef7c0e287b023256c71ac5c20c0646c3d4
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2024-12-30 17:24:49 +0100

    Cleaned up data

--- cleaning-data/main.py
+++ cleaning-data/main.py
@@ -1,1 +1,10 @@
 import json
+import re
+
+with open("data.json", "r") as text:
+    data = json.load(text)
+    for item in data:
+        item["Category"] = re.compile("[\.(]").split(item["Category"])[0]
+        
+print(data)
+    
