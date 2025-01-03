937d9f5292133c3c889488430e0d380d2e00d2b5
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2025-01-03 15:26:07 +0100

    Created a bubble graph about book ratings and number of pages

--- choose-your-own-data-visualization-adventure/main.py
+++ choose-your-own-data-visualization-adventure/main.py
@@ -4,5 +4,17 @@ import matplotlib.pyplot as plt
 
 with open("books.csv","r") as datafile:
     data = pd.read_csv(datafile,delimiter=",")
+    
+bins = [0, 2, 3, 4, 5]  # Define rating intervals
+labels = ["0-2", "2-3", "3-4", "4-5"]  # Labels for each bin
+data["rating_category"] = pd.cut(data["average_rating"], bins=bins, labels=labels, include_lowest=True)
 
-print(data["language_code"])
+sns.scatterplot(x="average_rating", y="# num_pages", size="average_rating", color="purple", 
+   sizes=(20, 180), alpha=0.8, data=data)         
+                
+plt.xlabel("Average Rating")
+plt.ylabel("Number of Pages")
+
+plt.title("Are longer books rated worse on GoodReads?")
+plt.savefig("book-ratings.png")
+
--- choose-your-own-data-visualization-adventure/solution.py
+++ choose-your-own-data-visualization-adventure/solution.py
@@ -4,6 +4,10 @@ import matplotlib.pyplot as plt
 
 with open("books.csv","r") as datafile:
     data = pd.read_csv(datafile,delimiter=",")
+    
+bins = [0, 2, 3, 4, 5]  # Define rating intervals
+labels = ["0-2", "2-3", "3-4", "4-5"]  # Labels for each bin
+data["rating_category"] = pd.cut(data["average_rating"], bins=bins, labels=labels, include_lowest=True)
 
 sns.scatterplot(x="average_rating", y="# num_pages", size="# num_pages", sizes=(20, 180), data=data)
 
